d9f525a50ab0fdd6ece2dc68bc928bef87541e03
harol <harolpardo26@gmail.com>
2025-05-29 18:47:15 -0500

    Mejoras en dashboard con datos reales

--- access/routes.py
+++ access/routes.py
@@ -9,6 +9,7 @@ templates = Jinja2Templates(directory="access/templates")  # Ruta donde están l
 @router.get("/admin", tags=["admin"])
 def home(request: Request):
     usuario_id = request.cookies.get("usuario_id")
+    
     if usuario_id:
         return templates.TemplateResponse("layout_adminv2.html", {"request": request})
     return RedirectResponse(url="/login?error=2", status_code=303)
