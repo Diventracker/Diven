abba71c4944b231ef559aad0f19e8e691bef5b8a
Sergio Mora <sergiomora14920@hotmail.com>
2025-07-03 02:34:39 -0500

    Generar informe por cliente

--- servicios/routes.py
+++ servicios/routes.py
@@ -54,7 +54,6 @@ def filtrar_clientes(search: str = "", db: Session = Depends(get_db)):
         (Cliente.numero_documento.ilike(f"%{search}%"))
     ).all()
 
-Ventasfix
     return [{"id": c.id_cliente, "nombre": c.nombre_cliente, "documento": c.numero_documento} for c in clientes]
 
 
